60a2df723d7c1ac4f407725f3eafa1cf31149328
ericness <ericness@users.noreply.github.com>
2024-04-09 12:30:20 +0000

    Finish styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const posts = [
+const post_list = [
     {
         name: "Vincent van Gogh",
         username: "vincey1853",
@@ -17,7 +17,7 @@ const posts = [
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
-        {
+    {
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
@@ -28,3 +28,32 @@ const posts = [
     }
 ]
 
+
+var posts = document.getElementById("posts");
+
+for (const post of post_list) {
+    var section = document.createElement("section");
+
+    section.innerHTML = `
+        <section class="post">
+            <section class="poster">
+                <img class="user-avatar" src="${post.avatar}">
+                <div class="poster-info">
+                    <p class="poster-name">${post.name}</p>
+                    <p class="poster-location">${post.location}</p>
+                </div>
+            </section>
+            <section class="post-image">
+                <img class="main-image" src="${post.post}">
+            </section>
+            <section class="comments">
+                <img class="icon" src="images/icon-heart.png">
+                <img class="icon" src="images/icon-comment.png">
+                <img class="icon" src="images/icon-dm.png">
+                <p class="likes">${post.likes} likes</p>
+                <p class="comment"><span class="commenter">${post.username}</span>${post.comment}</p>
+            </section>
+        </section>
+    `
+    posts.appendChild(section);
+}
